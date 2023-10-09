c088f6cb9d0f06c02b34087efb94f3417ea19fe1
Ankita <66182098+iamAnki@users.noreply.github.com>
2023-10-09 23:58:15 +0530

    Update task1.ts

--- task1.ts
+++ task1.ts
@@ -1,1 +1,20 @@
 // Find the smallest number in the array
+
+function findMinElement(arr: number[]): number | undefined {
+  if (arr.length === 0) {
+    return undefined; // Array is empty
+  }
+
+  const minElement = Math.min(...arr);
+  return minElement;
+}
+
+// Example usage:
+const numbers = [3, 8, 1, 6, 2, 9, 4, 7];
+const minNumber = findMinElement(numbers);
+
+if (minNumber !== undefined) {
+  console.log(`The smallest number in the array is: ${minNumber}`);
+} else {
+  console.log("The array is empty.");
+}
